ee7bf4fbbbf6f163e82ee395a402a114cceadf5c
pedrobzz <pedrobzz66@gmail.com>
2022-04-02 00:10:12 -0300

    Remoçao de coisas que não iremos usar
    Co-authored-by: Francielle Dellamora <francidellamora@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,51 +1,9 @@
 module.exports = {
   env: {
-    // apiKey: "",
-    // authDomain: "",
-    // projectId: "",
-    // storageBucket: "",
-    // messagingSenderId: "",
-    // appId: "",
-    // measurementId: "",
-    // firebase_admin_type: "",
-    // firebase_admin_project_id: "",
-    // firebase_admin_private_key_id: "",
-    // firebase_admin_private_key: "",
-    // firebase_admin_client_email: "",
-    // firebase_admin_client_id: "",
-    // firebase_admin_auth_uri: "",
-    // firebase_admin_token_uri: "",
-    // firebase_admin_auth_provider_x509_cert_url: "",
-    // firebase_admin_client_x509_cert_url:  "",
-    // firebase_admin_db_url: "",
-    apiKey: "AIzaSyBykSO9dGeKyBYuyXAcXEyO7k8t57ulQTw",
-    authDomain: "pbzz66.firebaseapp.com",
-    projectId: "pbzz66",
-    storageBucket: "pbzz66.appspot.com",
-    messagingSenderId: "695298633766",
-    appId: "1:695298633766:web:ce6cd8c883519a087636e7",
-    measurementId: "G-7GCDJ1105L",
-    firebase_admin_type: "service_account",
-    firebase_admin_project_id: "pbzz66",
-    firebase_admin_private_key_id: "a16e5ea5d7e82e3027cefd299bf21776e23eb3e4",
-    firebase_admin_private_key:
-      "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC7eD0/RbPYTLqN\n4cRnRhliGCWoNOWUmC4Tf4dacajmWbKaxR7H6pQSZSMcFW53jj1PSjsa0BewVjCo\nMXBLkff1A7D7a+3lzbyK1lvxDB/PHPcQa1+3T76GywjNw3r3kzHSP3rSgzw5lAXr\nHBrcSSmjTwlsOTRCeD92gkwZAJ6ZPC3OorzF6zQ+4N8IwayXfwwEINnYD6gfwFvV\nO+RsQdtdvJPFOHGu1CrADUgXENSQ0oM6BOIMnRSvYs+AkVbLg2Cg7r7kS52ajvt/\njYvvj5NYob98jIe/I4QV748CP0NUWAnD1f9xAEdHFXLH1L9r5FLbW/KiMt74bJbK\n/PJ9wW5/AgMBAAECggEAHdR3SRryD+AUVouO68Xw4mB9ls3P8tHiPF0FsG5h1ewo\n6iKlOrdeAgKmJbZX97MiQ1HIT4RpeI1xEZpISDtnoqIzO6vD/4CPWIzdCraHVeFB\n2OgqTf9k4uA/Zt+3hdnIoC//VFboiDBAXtWqhSnTySDERZG2pJWrYik/1ZdOeory\n7G4w8Ws7ayj6QeSZsdLCZFv34C4NfNC1p6BSuA90kOdt0virEbyl1d+i7AJa3VHe\nChxxBYvwWGZ3oSATVufl9xZUKoahxd7ALi7E+5+9LrAzBJcCEHvJys9PARL2cOuR\ngJ0A/AkqiAK+gUMDSta1IcU+FuCOmo5tms6J6DTKiQKBgQDy73rHQ2o4Oj7WCiP2\nDmSegPQmwHm4MsCMbmHgcZUtSPmeJ6rnFFhRtu2lwvQteyKc7mdgdfb54JJCVpj1\nJrderAR6bZ+nXIhLwhXRMNcFhkHAtXvYtNDHMPX3+7NSMT2vmVyVT85XoKebq/TM\nr+80B2IC7IdVn1Fc1Aj3BqHbSQKBgQDFjSfml/ex02cWSn8BGcI9H6T/8WKhR0f7\nBmyRbcBbYA6nikUZyHurirrsVOxlkEIWb7C5iBeiPWqHkZDOb81hpxqwIWK4mZa+\nBWF+BqKzuRSLEDhiOyq2WYjlBTKAcsKBgVHnoX8FqXuUPufX6gHUTFFunLukHJzt\nM+JNRL1zhwKBgQDGVLzn8SHGxcaG98BkIVwEC0NIX9r/hm+sNnGxywj+75TaiYf9\nH577F1zs9zxh5SVc+UVseuInHjBJ+Gjnu7P4lncRfG3bNLARKWKmZjklSXZk3cU+\nJmBMPqyQx+yeLZUuxwXO4FiVMZOkg75XXUrsLxrVSEU0odzK7FBlXATIAQKBgQCC\n0dn1avc9DBh8+3IlQXCOV5FFaAz/bhutkPB+UaaLHA44RmlCDLBQxbrI2+f7zc7l\n2qq2AfhcmL0d73hsLD9ehnE257TfN95KEXghI7XEvDBtcp1T94huXtY3ivyJcPV7\nGy8o65oZHsXWQt327mCtvKhETog3wjbtIWmf74BhqwKBgHhLcng/AMzwYd9ifGbm\nMvDZKEfrgX6Fs/jUQbkP0LfJ2bl0ydXVaLXj0kzLH9hH2zPLYgWxg3C1/+PXQyIV\nhmP4t3oQANHX+FUp9DuzF+dQE78qJlOJuvR0aSGYcmEFmWMMiJpY3kKEggGhE0TD\nrAZTLsyJaO+aVEbng5xcvf+i\n-----END PRIVATE KEY-----\n",
-    firebase_admin_client_email:
-      "firebase-adminsdk-91b8x@pbzz66.iam.gserviceaccount.com",
-    firebase_admin_client_id: "103972615668917568277",
-    firebase_admin_auth_uri: "https://accounts.google.com/o/oauth2/auth",
-    firebase_admin_token_uri: "https://oauth2.googleapis.com/token",
-    firebase_admin_auth_provider_x509_cert_url:
-      "https://www.googleapis.com/oauth2/v1/certs",
-    firebase_admin_client_x509_cert_url:
-      "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-91b8x%40pbzz66.iam.gserviceaccount.com",
-    firebase_admin_db_url:
-      "https://1:148256582490:web:65769b1a208d01d318e163.firebaseio.com",
-    mongo_db_url:
-      "mongodb+srv://yungbzz:pbzz66@commerce.bhazs.mongodb.net/test?authSource=admin&replicaSet=atlas-tkwbv6-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true",
+    mongo_db_url: "",
   },
   images: {
-    domains: ["lh3.googleusercontent.com", "assets.adidas.com"],
+    domains: [],
   },
 
   webpack(config) {
